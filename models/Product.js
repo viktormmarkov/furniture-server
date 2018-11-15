796b61c04544ccf8fcc0772f003b3d884d6b7e12
Victor Markov <vikata3@gmail.com>
2018-11-16 00:17:56 +0200

    Init commit

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    name: String,
+    price: Number,
+    category: String,
+    image: String,
+    description: String,
+    vendor: String
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
